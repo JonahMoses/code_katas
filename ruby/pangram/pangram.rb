424cdffe6403fa1585d08f2bbc2ea6b3db16871f
Jonah Moses <jmoses@newrelic.com>
2017-05-23 22:51:17 -0700

    complete pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,4 +1,14 @@
 class Pangram
+
   def self.pangram?(phrase)
+    letters = phrase.downcase.split("").select { |char| ('a'..'z').include?(char) }
+    ('a'..'z').all? { |char| letters.include?(char) }
+
+    # phrase.downcase.split("").select { |char| ('a'..'z').include?(char) }.uniq.size == 26
   end
+
+end
+
+module BookKeeping
+  VERSION = 4 # Where the version number matches the one in the test.
 end
